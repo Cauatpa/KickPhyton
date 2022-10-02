eff3de793ab64fbc05e85010be4da1a581d5d0b3
Cauã Thurler <101412705+Cauatpa@users.noreply.github.com>
2022-10-02 20:24:56 -0300

    add desafio

---
+++ Desafio 42/index.py
@@ -0,0 +1,33 @@
+#1 - Faça um programa em que o usuário digita dois valores e o resultado da soma é exibido na tela.
+
+v1 = input("Digite um valor: ")
+v2 = input("Digite outro valor: ")
+
+print(int(v1) + int(v2))
+
+#2 - Faça um programa em que o usuário precisa cadastrar nome, idade, telefone e e-mail. Depois mostre os valores digitados na tela.
+
+nome = input("Nome :")
+idade = input("Idade: ")
+telefone = input("Telefone: ")
+email = input("E-mail: ")
+
+
+print(nome + ", ", idade + ", ", telefone + ", ", email + ", ")
+
+#3 Faça um programa no qual o usuário precisa cadastrar as informações de um produto: código, nome, quantidade e preço. No final o programa deve mostrar as informações
+
+
+
+#4 - Faça um programa que converte centímetros em metros
+
+vv1 = input("Digite quantos cm vc quer converter em metros: ")
+
+print(int(vv1) / 100)
+
+#5 Faça um programa que calcule a área de um quadrado/retângulo. 
+
+base = input("Escolha á base do quadrado: ")
+altura = input("Escolha a altura do quadrado: ")
+
+print(int(base) * int(altura))
